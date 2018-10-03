325441dff54277ae34979b0621ca8b863e04d0c4
Saurabh Maheshwari <saurabh.maheshwari@accenture.com>
2018-10-02 19:47:04 -0700

    adding code

--- build.sbt
+++ build.sbt
@@ -8,5 +8,6 @@ libraryDependencies +=
   "org.typelevel" %% "cats-core" % "1.0.0"
 scalacOptions ++= Seq(
   "-Xfatal-warnings",
-  "-Ypartial-unification"
+  "-Ypartial-unification",
+  "-language:higherKinds"
 )
--- src/main/scala/com/heliosmi/fp/MonoidCats.scala
+++ src/main/scala/com/heliosmi/fp/MonoidCats.scala
@@ -1,17 +1,43 @@
 package com.heliosmi.fp
 
-trait Monoid[A] {
-def combine(x:A, y:A) :A
+
+trait Semigroup[A] {
+  def combine(x: A, y: A): A
+}
+
+trait Monoid[A] extends Semigroup[A] {
 def empty: A
 }
 
-object MonoidCats{
- def associativeLaw[A](x: A, y:A , z: A) (implicit m: Monoid[A]) : Boolean = {
-  m.combine(x, m.combine(y,z)) == m.combine(m.combine(x,y),z)
+object MonoidCats {
+  def associativeLaw[A](x: A, y: A, z: A)(implicit m: Monoid[A]): Boolean = {
+    m.combine(x, m.combine(y, z)) == m.combine(m.combine(x, y), z)
 
- }
+  }
 
- def identityLaw[A](x:A)(implicit m:Monoid[A]): Boolean = {
-  (m.combine(x, m.empty) == x) && (m.combine(m.empty, x) == x)
- }
+  def identityLaw[A](x: A)(implicit m: Monoid[A]): Boolean = {
+    (m.combine(x, m.empty) == x) && (m.combine(m.empty, x) == x)
+  }
+}
+
+
+object AdderExercise{
+  import cats.Monoid._
+  import cats.syntax.semigroup._
+  def add(items: List[Int]):Int = items.fold(0)( _ + _ )
+
+  def add[A](items: List[A])(implicit m: cats.Monoid[A]) : A =
+    items.fold(m.empty)( _ |+| _)
+
+  case class Order(totalCost: Double, quantity: Double)
+
+  def monoidOrder = new cats.Monoid[Order]{
+     def empty: Order = Order(0,0)
+
+     def combine(x: Order, y: Order): Order = Order(
+       x.totalCost + y.totalCost,
+       x.quantity + y.quantity
+     )
+
+  }
 }
